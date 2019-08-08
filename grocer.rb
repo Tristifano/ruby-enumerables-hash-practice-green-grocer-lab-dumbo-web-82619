da78e9dbd1d826ba48427633268ff3864f9d56be
Tristan Myers <tristifano@gmail.com>
2019-08-08 12:41:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,46 @@
 def consolidate_cart(cart)
-  # code here
+  new_cart = {}
+  cart.each {|object|
+    object.each {|item,info|
+      if new_cart[item] 
+        new_cart[item][:count] += 1
+      else
+        new_cart[item] = info
+        new_cart[item][:count] = 1
+      end
+    }
+  }
+  new_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+ 
+    coupons.each { |deal|
+    item = deal[:item]
+    couponed_item = "#{item} W/COUPON"
+    discounted_item_cost = deal[:cost] / deal [:num]
+    original_item = cart[item]
+    coupon_num = deal[:num]
+    if cart.include?(item) && cart.include?(couponed_item)
+      cart_count = original_item[:count]
+      updated_count = cart_count - coupon_num
+      original_item[:count] = updated_count
+      cart[couponed_item][:count] = cart[couponed_item][:count] + deal[:num]
+    elsif cart.include?(item)
+    cart_count = original_item[:count]
+    updated_count = cart_count - coupon_num
+    original_item[:count] = updated_count
+      cart[couponed_item] = {:price => discounted_item_cost, :clearance => original_item    [:clearance], :count => deal[:num] }
+    end
+    }
+    cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.map {|item|
+    if item[:clearance]
+      item[:price] = item[:price] * .20
+  }
 end
 
 def checkout(cart, coupons)
